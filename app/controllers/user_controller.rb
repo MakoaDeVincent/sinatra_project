b113e29d8757fce23fb8b722135bb0d7fb0b3e03
MakoaDeVincent <makoadevincent@gmail.com>
2020-07-29 17:58:09 -1000

    WIP: Set up Log in

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,9 @@
 class UserController < ApplicationController
 
     get '/signup' do
-        #if logged_in?
-            #redirect '/vinyl_records'
+        if logged_in?
+            redirect '/vinyl_records'
+        end
         erb :'/users/new'
     end
 
@@ -16,6 +17,28 @@ class UserController < ApplicationController
         end
     end
 
+    get '/login' do
+        if logged_in?
+            redirect "/vinyl_records"
+        end
+        
+        erb :'/users/login'
+    end
 
+    post '/login' do
+        @user = User.find_by(:username => params[:username], :email => params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect '/vinyl_records'
+        else
+            erb :'/users/login'
+        end
+    end
 
+    get '/logout' do
+        if logged_in?
+            session.clear
+        end
+        redirect '/'
+    end
 end
