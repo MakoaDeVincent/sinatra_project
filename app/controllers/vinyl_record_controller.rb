a442f21dd23cb2de3e2f0b5d97024675a0b8e620
MakoaDeVincent <makoadevincent@gmail.com>
2020-07-31 14:36:55 -1000

    WIP:create vinyl records

--- app/controllers/vinyl_record_controller.rb
+++ app/controllers/vinyl_record_controller.rb
@@ -1,7 +1,22 @@
 class VinylRecordController < ApplicationController
 
     get '/vinyl_records' do
-        erb :'vinyl_records/vinyl_records'
+        @user = current_user
+        erb :'vinyl_records/index'
+    end
+
+    get '/vinyl_records/new' do
+        erb :'vinyl_records/new'
+    end
+
+    post '/vinyl_records' do
+        @vinyl_record = current_user.vinyl_records.create(params)
+
+        if @vinyl_record.save
+            redirect '/vinyl_records'
+        else
+            redirect '/vinyl_records/new'
+        end 
     end
 
 end
