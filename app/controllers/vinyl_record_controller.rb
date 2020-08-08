871314d7664fcdf8c0575dba250e7d5a93b5b581
MakoaDeVincent <makoadevincent@gmail.com>
2020-08-07 16:09:01 -1000

    Added Update and Delete

--- app/controllers/vinyl_record_controller.rb
+++ app/controllers/vinyl_record_controller.rb
@@ -19,4 +19,37 @@ class VinylRecordController < ApplicationController
         end 
     end
 
+    get '/vinyl_records/:id' do
+        @vinyl_record = VinylRecord.find_by_id(params[:id])
+            if @vinyl_record == nil
+                redirect '/vinyl_records'
+            end
+
+        erb :'vinyl_records/show'
+    end
+
+    get '/vinyl_records/:id/edit' do
+        @vinyl_record = VinylRecord.find_by_id(params[:id])
+
+        erb :'vinyl_records/edit'
+    end
+
+    patch '/vinyl_records/:id' do
+        @vinyl_record = VinylRecord.find_by_id(params[:id])
+
+        @vinyl_record.update(artist: params[:artist], album: params[:album], genre: params[:genre], year: params[:year])
+
+        redirect '/vinyl_records'
+    end
+
+    delete '/vinyl_records/:id' do
+        if !logged_in?
+            redirect '/login'
+        end
+        VinylRecord.delete(params[:id])
+
+        redirect '/vinyl_records'
+    end
+
+
 end
