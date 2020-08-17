3ae6f1749bc2b575d7816b3fde979be0ac16efdb
MakoaDeVincent <makoadevincent@gmail.com>
2020-08-16 18:34:59 -1000

    User can only edit their own records.

--- app/controllers/vinyl_record_controller.rb
+++ app/controllers/vinyl_record_controller.rb
@@ -10,17 +10,18 @@ class VinylRecordController < ApplicationController
     end
 
     post '/vinyl_records' do
-        @vinyl_record = current_user.vinyl_records.create(params)
+        @vinyl_record = current_user.vinyl_records.new(params)
 
         if @vinyl_record.save
             redirect '/vinyl_records'
         else
-            redirect '/vinyl_records/new'
+            erb :'/vinyl_records/new'
         end 
     end
 
     get '/vinyl_records/:id' do
-        @vinyl_record = VinylRecord.find_by_id(params[:id])
+        #@vinyl_record = VinylRecord.find_by_id(params[:id])
+        @vinyl_record = current_user.vinyl_records.find_by_id(params[:id])
             if @vinyl_record == nil
                 redirect '/vinyl_records'
             end
@@ -29,13 +30,15 @@ class VinylRecordController < ApplicationController
     end
 
     get '/vinyl_records/:id/edit' do
-        @vinyl_record = VinylRecord.find_by_id(params[:id])
+        #@vinyl_record = VinylRecord.find_by_id(params[:id])
+        @vinyl_record = current_user.vinyl_records.find_by_id(params[:id])
 
         erb :'vinyl_records/edit'
     end
 
     patch '/vinyl_records/:id' do
-        @vinyl_record = VinylRecord.find_by_id(params[:id])
+        #@vinyl_record = VinylRecord.find_by_id(params[:id])
+        @vinyl_record = current_user.vinyl_records.find_by_id(params[:id])
 
         @vinyl_record.update(artist: params[:artist], album: params[:album], genre: params[:genre], year: params[:year])
 
